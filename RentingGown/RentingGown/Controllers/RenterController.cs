e5fbae85a352c0909674e0fd3993faa061e28e26
tamarfrishman <tamarf1997@gmail.com>
2018-10-04 15:30:08 +0300

    fg

---
+++ RentingGown/RentingGown/Controllers/RenterController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RentingGown.Controllers
+{
+    public class RenterController : Controller
+    {
+        // GET: Renter
+        public ActionResult Renter()
+        {
+            return View();
+        }
+    }
+}
---
+++ RentingGown/RentingGown/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using RentingGown.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RentingGown.Controllers
+{
+    public class UserController : Controller
+    {
+        private RentingGownDB db = new RentingGownDB();
+        // GET: User
+        public ActionResult Login(string name,string password)
+        {
+            Users user = db.Users.FirstOrDefault(u => u.username == name && u.password == password);
+            if (user != null)
+            {
+                if (user.status == 1)//מנהל
+                    return RedirectToAction("Manager", "Manager");
+                //משכיר
+                return RedirectToAction("Renter", "Renter");
+            }
+            return View();
+        }
+    }
+}
